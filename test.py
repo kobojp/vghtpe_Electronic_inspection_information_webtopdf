1a05712c115415364c98415b1cf27aa1a183a56c
kobojp <lovermk520@gmail.com>
2022-06-19 23:39:07 +0800

    UI開發

--- test.py
+++ test.py
@@ -1,56 +1,229 @@
-import json
-import pdfkit
-import sys
-from PyQt5 import QtWidgets, QtWebEngineWidgets
-from PyQt5.QtCore import QUrl
-from PyQt5.QtGui import QPageLayout, QPageSize
-from PyQt5.QtWidgets import QApplication
+# import os
+# import subprocess
+ 
+ 
+# # 利用subprocess
+# def startfile(filename):
+#     try:
+#         os.startfile(filename)
+#     except:
+#         subprocess.Popen(['xdg-open', filename])
+ 
+ 
+# start_directory = r'C:\my_code\vghtpe_Electronic_inspection_information_webtopdf\test'
 
-def topdf(url,file_name):
-    app = QtWidgets.QApplication(sys.argv)
-    loader = QtWebEngineWidgets.QWebEngineView()
-    loader.setZoomFactor(1)
-    layout = QPageLayout()
-    layout.setPageSize(QPageSize(QPageSize.A4Extra))
-    layout.setOrientation(QPageLayout.Portrait)
-    loader.load(QUrl(url))
-    loader.page().pdfPrintingFinished.connect(lambda *args: QApplication.exit())
 
-    def emit_pdf(finished):
-        loader.page().printToPdf(file_name +'.pdf', pageLayout=layout)
+# startfile(start_directory)
 
-    loader.loadFinished.connect(emit_pdf)
-    sys.exit(app.exec_())
+# from tkinter import ttk
+# import tkinter as tk
+ 
+# app = tk.Tk() 
+# app.geometry('200x100')
 
-    
+# labelTop = tk.Label(app,
+#                     text = "Choose your favourite month")
+# labelTop.grid(column=0, row=0)
 
-# with open("data.json", encoding="utf-8") as f:
+# comboExample = ttk.Combobox(app, 
+#                             values=[
+#                                     "January", 
+#                                     "February",
+#                                     "March",
+#                                     "April"])
+# print(dict(comboExample)) 
+# comboExample.grid(column=0, row=1)
+# comboExample.current(1)
 
-#     # 讀取 JSON 檔案
-#     p = json.load(f)
-    # print(p)
+# print(comboExample.current(), comboExample.get())
 
-    # 查看整個 JSON 資料解析後的結果
-    # print("p =", p)
-    # print("p =", p['members'][0]['name'])
+# app.mainloop()
 
-# date = '2022-05'
-# for i in p['Fire_Equipment']:
-#     s = i['name']
-#     b = i['api_1']
-#     c = i['api_2']
-#     print(f'http://210.61.217.104/Report6{b}{date}{c}' + f'  {s}')
-#     url = f'http://210.61.217.104/Report6{b}{date}{c}'
+# win = tk.Tk()
+# win.title("Python GUI")    # 添加标题
 
-#     pdfkit.from_url(url, s + '.pdf')
-    # topdf(url,s)
-pdfkit.from_url('http://210.61.217.104/Report6/63/2022-05/63/79', 'test' + '.pdf')
+# print(win.winfo_screenwidth()) #輸出螢幕寬度
+# print(win.winfo_screenheight()) #輸出螢幕高度
+# w=800  #width
+# r=300  #height
+# x=200  #與視窗左上x的距離
+# y=300  #與視窗左上y的距離
+# win.geometry('%dx%d+%d+%d' % (w,r,x,y))
+ 
+# ttk.Label(win, text="Chooes a number").grid(column=1, row=0)    # 添加一个标签，并将其列设置为1，行设置为0
+# ttk.Label(win, text="Enter a name:").grid(column=0, row=0)      # 设置其在界面中出现的位置  column代表列   row 代表行
+ 
+# # button被点击之后会被执行
+# def clickMe():   # 当acction被点击时,该函数则生效
+#   action.configure(text='Hello ' + name.get()+ ' ' + numberChosen.get())     # 设置button显示的内容
+#   action.configure(state='disabled')      # 将按钮设置为灰色状态，不可使用状态
+ 
+# # 按钮
+# action = ttk.Button(win, text="Click Me!", command=clickMe)     # 创建一个按钮, text：显示按钮上面显示的文字, command：当这个按钮被点击之后会调用command函数
+# action.grid(column=2, row=1)    # 设置其在界面中出现的位置  column代表行   row 代表列
+ 
+# # 文本框
+# name = tk.StringVar()     # StringVar是Tk库内部定义的字符串变量类型，在这里用于管理部件上面的字符；不过一般用在按钮button上。改变StringVar，按钮上的文字也随之改变。
+# nameEntered = ttk.Entry(win, width=12, textvariable=name)   # 创建一个文本框，定义长度为12个字符长度，并且将文本框中的内容绑定到上一句定义的name变量上，方便clickMe调用
+# nameEntered.grid(column=0, row=1)       # 设置其在界面中出现的位置  column代表列   row 代表行
+# nameEntered.focus()     # 当程序运行时,光标默认会出现在该文本框中
+ 
+# # 创建一个下拉列表
+# number = tk.StringVar()
+# numberChosen = ttk.Combobox(win, width=12, textvariable=number)
+# numberChosen['values'] = (1, 2, 4, 42, 100)     # 设置下拉列表的值
+# numberChosen.grid(column=1, row=1)      # 设置其在界面中出现的位置行  column代表   row 代表列
+# numberChosen.current(0)    # 设置下拉列表默认显示的值，0为 numberChosen['values'] 的下标值
 
-# pdfkit.from_url('http://210.61.217.104/Report6/86/2022-05/86/102', 'outAA.pdf')
-        # print(s,b,c)
-    
-    # # 取得 name 的值
-    # print("name =", p["name"])
 
-    # # 取得 skill 的值
-    # print("skill =", p["skill"])
+# win.mainloop()      # 当调用mainloop()时,窗口才会显示出来
+
+
+from tkinter import *
+
+# print(到視窗)
+# def check_expression():
+#     #Your code that checks the expression
+#     varContent = inputentry.get() # get what's written in the inputentry entry widget
+#     outputtext.delete('0', 'end-1c') # clear the outputtext text widget
+#     outputtext.insert(varContent)
+
+# root = Tk()
+# root.title("Post-fix solver")
+# root.geometry("500x500")
+
+# mainframe = Frame(root)
+# mainframe.grid(column=0, row=0)
+
+# inputentry = Entry(mainframe)
+# inputentry.grid(column=1, row=1)
+
+# executebutton = Button(mainframe, text="Run", command=check_expression)
+# executebutton.grid(column=1, row=5)              
+
+# outputtext = Text(mainframe)
+# outputtext.grid(column=1, row=5)
+
+# root.mainloop()
+
+# from tkinter import *
+# from tkinter import ttk
+# root = Tk()
+# frm = ttk.Frame(root, padding=10)
+# frm.grid()
+# ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+# root.mainloop()
+
+
+# 以這個版本去修改，日期部分選擇值後取得選擇值，按下按鈕執行 爬取函式，將print結果丟到日誌窗
+
+import hashlib
+import time
+
+LOG_LINE_NUM = 0
+
+class MY_GUI():
+    def __init__(self,init_window_name):
+        self.init_window_name = init_window_name
+
+
+    #设置窗口
+    def set_init_window(self):
+        self.init_window_name.title("文本处理工具_v1.2")           #窗口名
+        #self.init_window_name.geometry('320x160+10+10')                         #290 160为窗口大小，+10 +10 定义窗口弹出时的默认展示位置
+        self.init_window_name.geometry('1300x900+10+10')
+        #self.init_window_name["bg"] = "pink"                                    #窗口背景色，其他背景色见：blog.csdn.net/chl0000/article/details/7657887
+        #self.init_window_name.attributes("-alpha",0.9)                          #虚化，值越小虚化程度越高
+        #标签
+        self.init_data_label = Label(self.init_window_name, text="待处理数据")
+        self.init_data_label.grid(row=0, column=0)
+        self.result_data_label = Label(self.init_window_name, text="输出结果")
+        self.result_data_label.grid(row=0, column=12)
+        self.log_label = Label(self.init_window_name, text="日志")
+        self.log_label.grid(row=12, column=0)
+        self.select = Label(self.init_window_name, text="選擇框")
+        self.select.grid(row=15, column=0)
+
+
+        #文本框
+        self.init_data_Text = Text(self.init_window_name, width=67, height=35)  #原始数据录入框
+        self.init_data_Text.grid(row=1, column=0, rowspan=10, columnspan=10)
+
+
+        self.result_data_Text = Text(self.init_window_name, width=70, height=49)  #处理结果展示
+        self.result_data_Text.grid(row=1, column=12, rowspan=15, columnspan=10)
+        self.log_data_Text = Text(self.init_window_name, width=66, height=9)  # 日志框
+        self.log_data_Text.grid(row=13, column=0, columnspan=10)
+
+        #按钮
+        self.str_trans_to_md5_button = Button(self.init_window_name, text="字符串转MD5", bg="lightblue", width=10,command=self.str_trans_to_md5)  # 调用内部方法  加()为直接调用
+        self.str_trans_to_md5_button.grid(row=1, column=11)
+        
+        #選擇框
+        self.select_option = Listbox(self.init_window_name)
+        self.select_option.grid(row=18, column=0, columnspan=10)
+
+        # 顯示清單
+        # https://www.geeksforgeeks.org/how-to-get-selected-value-from-listbox-in-tkinter/
+        # 取得選擇值寫法
+
+        li = ['C','python','php','html','SQL','java']
+        for item in li:            # 第一个小部件插入数据
+            self.select_option.insert(0,item) 
+        # self.select_option.pack()
+
+    #功能函数
+    def str_trans_to_md5(self):
+
+        
+        src = self.init_data_Text.get(1.0,END).strip().replace("\n","").encode()
+        #print("src =",src)
+        if src:
+            try:
+                myMd5 = hashlib.md5()
+                myMd5.update(src)
+                myMd5_Digest = myMd5.hexdigest()
+                #print(myMd5_Digest)
+                #输出到界面
+                self.result_data_Text.delete(1.0,END)
+                self.result_data_Text.insert(1.0,myMd5_Digest)
+                self.write_log_to_Text("INFO:str_trans_to_md5 success")
+            except:
+                self.result_data_Text.delete(1.0,END)
+                self.result_data_Text.insert(1.0,"字符串转MD5失败")
+        else:
+            self.write_log_to_Text("ERROR:str_trans_to_md5 failed")
+
+
+    #获取当前时间
+    def get_current_time(self):
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        return current_time
+
+
+    #日志动态打印
+    def write_log_to_Text(self,logmsg):
+        global LOG_LINE_NUM
+        current_time = self.get_current_time()
+        logmsg_in = str(current_time) +" " + str(logmsg) + "\n"      #换行
+        if LOG_LINE_NUM <= 7:
+            self.log_data_Text.insert(END, logmsg_in)
+            LOG_LINE_NUM = LOG_LINE_NUM + 1
+        else:
+            self.log_data_Text.delete(1.0,2.0)
+            self.log_data_Text.insert(END, logmsg_in)
+
+
+def gui_start():
+    init_window = Tk()              #实例化出一个父窗口
+    ZMJ_PORTAL = MY_GUI(init_window)
+    # 设置根窗口默认属性
+    ZMJ_PORTAL.set_init_window()
+
+    init_window.mainloop()          #父窗口进入事件循环，可以理解为保持窗口运行，否则界面不展示
+
+
+gui_start()
+
+
