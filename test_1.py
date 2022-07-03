a3d03bbc8b0aa990d6c2462571ba011e8130f261
kobojp <lovermk520@gmail.com>
2022-07-03 19:10:03 +0800

    update

--- test_1.py
+++
@@ -1,8 +0,0 @@
-
-import pdfkit
-import os
-
-path_wkhtmltopdf = os.path.join('wkhtmltopdf.exe')
-config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-
-pdfkit.from_url('https://www.google.com/', '123.pdf', configuration=config)
